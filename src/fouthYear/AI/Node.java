0b68d16fafa354edf2a1444738b62b64fc2890c0
salma-abd-elaziz <salma-abd-elaziz@users.noreply.github.com>
2017-10-27 16:10:28 +0200

    class Node is finished

--- src/fouthYear/AI/Node.java
+++ src/fouthYear/AI/Node.java
@@ -1,5 +1,32 @@
 package fouthYear.AI;
 
 public class Node {
-
+	// Number of attacks in the state which is the value of the node.
+	private int numberOfAttacks;
+	private State state;
+	
+	// Called to generate the initial state.
+	public Node() {
+		numberOfAttacks = 0;
+		state = new State(); // Generate a random state.
+		// Calculate its number of attacks.
+		numberOfAttacks = state.calculatenumberOfAttacks();
+	}
+	
+	public Node (State state, int numberOfAttacks){
+		this.state = state;
+		this.numberOfAttacks = numberOfAttacks;
+	}
+	
+	public int getNumberOfAttacks() {
+		return numberOfAttacks;
+	}
+	
+	/*
+	 * the function calls a function on State class to find the best neighbor
+	 * @returns a node which contain the state and the value of the best 
+	 * 		successor*/
+	public Node getBestSuccessor(){
+		return state.neighborWithlowestAttachNo();
+	}
 }
--- src/fouthYear/AI/State.java
+++ src/fouthYear/AI/State.java
@@ -5,15 +5,11 @@ import java.util.Random;
 public class State {
 	// The index number is the row number, the value is the column number. 
 	private Queen[] stateBoard;
-	// Number of attacks in the state.
-	private int numberOfAttacks;
 	
 
 	public State(){
 		stateBoard = new Queen[Luncher.queensNumber];
-		numberOfAttacks = 0;
 		this.genStateRan();
-		this.calculatenumberOfAttacks();
 	}
 
 	public State(State state){
@@ -32,9 +28,6 @@ public class State {
 		return stateBoard[rowNo];
 	}
 
-	public int getNumberOfAttacks() {
-		return numberOfAttacks;
-	}
 
 	/* The function generates the sate at random, only one queen in each row. */
 	private void genStateRan(){
@@ -58,33 +51,37 @@ public class State {
 			queen = state.getQueenInRow(i);
 			stateBoard[i] = new Queen(i, queen.getColunmnNumber()); 
 		}
-		this.numberOfAttacks = state.getNumberOfAttacks();
 	}
 	
 	/* The function finds all the neighbors of the state (64 board) 
 	 * and returns the one with minimum number of attacks*/
-	public State neighborWithlowestAttachNo() {
+	public Node neighborWithlowestAttachNo() {
 		State result = null;
+		int resultNoAttacks = Integer.MAX_VALUE;
 		for (int i = 0; i < Luncher.queensNumber; i++) {
 			for (int j = 0; j < Luncher.queensNumber; j++) {
 				State neighbor = new State(this); 
 				// Move the i(th) queen by j steps for 8 times. 
 				neighbor.getQueenInRow(i).moveRight(j); 
-				neighbor.calculatenumberOfAttacks();
-				if (result != null && neighbor.getNumberOfAttacks() < result.getNumberOfAttacks()) {
+				int neighborNoAttacks = neighbor.calculatenumberOfAttacks();
+				// Update the result with the neighbor with the smaller 
+				//number of attacks.
+				if (neighborNoAttacks < resultNoAttacks) {
 					result = neighbor;
+					resultNoAttacks = neighborNoAttacks;
 				}
 			}
 		}
-		return result;
+		return new Node(result, resultNoAttacks);
 	}
 
 	/* The function calculate the number of attacks of the state.
 	 * @return the number of attacks it calculated*/
 	public int calculatenumberOfAttacks() {
+		int numberOfAttacks = 0;
 		for (int i = 0; i < Luncher.queensNumber; i++) {
 			for (int j = i + 1; j < Luncher.queensNumber; j++) {
-				if (stateBoard[i].isAttacking(stateBoard[j])) this.numberOfAttacks++;
+				if (stateBoard[i].isAttacking(stateBoard[j])) numberOfAttacks++;
 			}
 		}
 		return numberOfAttacks;
